88150c7446f5634366b670e030c64e55ae839c63
izhukov <izhukov@btlab.ru>
2021-11-12 18:20:37 +0300

    multiple windows

--- chromeDriver/main.py
+++ chromeDriver/main.py
@@ -12,16 +12,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 from selenium.common.exceptions import InvalidCookieDomainException
 
-from proxy_auth_data import login as proxy_login, password as proxy_password, server as proxy_server
+from multiprocessing import Pool
+
+# from proxy_auth_data import login as proxy_login, password as proxy_password, server as proxy_server
+from proxy_auth_data import proxy
 from rutracker_auth_data import login as tracker_login, password as tracker_password
 
-url = "https://rutracker.org/forum/tracker.php?f=2387"
 
-proxy_options = {
-    "proxy": {
-        "https": f"http://{proxy_login}:{proxy_password}@{proxy_server}"
-    }
-}
 
 service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
@@ -29,12 +26,8 @@ options.add_argument(f"user-agent={UserAgent().chrome}")
 options.add_argument("--disable-blink-features=AutomationControlled")
 # options.add_argument("--headless")
 
-driver = webdriver.Chrome(
-    service=service,
-    seleniumwire_options=proxy_options,
-    options=options
-)
-
+auth_url = "https://rutracker.org/forum/login.php"
+url = "https://rutracker.org/forum/tracker.php?f=2387"
 matches = {
     "year": "Год выпуска",
     "last_name": "Фамилия автора",
@@ -55,9 +48,9 @@ matches = {
 }
 
 results = []
+drivers = []
 
-
-def login_to_site():
+def login_to_site(driver, url):
     cookie_file_name = f"{tracker_login}_cookies"
     cookie_path = Path(cookie_file_name)
     need_auth = False
@@ -71,14 +64,15 @@ def login_to_site():
         need_auth = True
 
     if need_auth:
-        auth()
+        auth(driver)
         pickle.dump(driver.get_cookies(), open(cookie_file_name, "wb"))
     if driver.current_url != url:
         driver.get(url)
     time.sleep(1)
 
 
-def auth():
+def auth(driver):
+    driver.get(auth_url)
     login_input = driver.find_element(By.XPATH, "//input[@name='login_username'][not(@id='top-login-uname')]")
     login_input.clear()
     login_input.send_keys(tracker_login)
@@ -90,23 +84,33 @@ def auth():
     time.sleep(1)
 
 
-def get_search_pages():
+def get_search_pages(driver):
     elems = driver.find_elements(By.CLASS_NAME, "pg")
-    return set(map(lambda el: el.get_attribute("href"), elems))
+    result = remove_duplicates(list(map(lambda el: [el.text, el.get_attribute("href")], elems)))
+    result["1"] = driver.current_url
+    # result.append(driver.current_url)
+    return result
 
 
-def open_new_tab(page_url):
+def remove_duplicates(data):
+    result = {}
+    for el in data:
+        if result.get(el[0]) is not None:
+            result[el[0]] = el[1]
+    return result
+
+
+def open_new_tab(driver, page_url):
     driver.execute_script(f"window.open('{page_url}');")
 
 
-def close_current_tab():
-    if len(driver.window_handles) < 2:
-        open_new_tab("")
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
+def close_tab(driver):
+    if len(driver.window_handles) > 1:
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
 
 
-def get_rows_data():
+def get_rows_data(driver):
     elems = driver.find_elements(By.XPATH, "//tr[starts-with(@id,'trs-tr-')]/td[4]/div[1]/a")
     return list(map(lambda el: {
         "href": el.get_attribute("href"),
@@ -121,7 +125,7 @@ def get_book_data_by_type(root_item, book_data_type):
         if item.text == book_data_type:
             if book_data_type == "Описание":
                 val = ""
-                for j in range(i+1, items.__len__()-1):
+                for j in range(i+1, len(items)-1):
                     if type(items[j]) == bs4.element.NavigableString:
                         val += items[j].text + "\n\n"
                     else:
@@ -140,8 +144,9 @@ def cut_off_excess(val):
     return val.strip()
 
 
-def get_book_data(row_data):
-    soup = bs(driver.page_source)
+def get_book_data(driver, row_data):
+    time.sleep(1)
+    soup = bs(driver.page_source, features="html.parser")
     root_item = soup.find('div', attrs={'class': 'post_body'})
     book_data = {
         "url": row_data["href"],
@@ -153,36 +158,90 @@ def get_book_data(row_data):
     return book_data
 
 
-def get_books_data(page_url):
+def get_books_data(driver):
     result = []
-    if page_url is not None:
-        driver.get(page_url)
-    # for row_data in get_rows_data():
-    #     driver.get(row_data["href"])
-    #     result.append(get_book_data(row_data))
-    close_current_tab()
+    # driver.get(page_url)
+    # for row_data in get_rows_data(driver):
+    row_data = get_rows_data(driver)[0]
+    driver.get(row_data["href"])
+    result.append(get_book_data(driver, row_data))
+    close_tab(driver)
     return result
 
 
-def get_all_books_data():
-    search_pages = get_search_pages()
-    result = get_books_data(None)
-    for page_url in search_pages:
-        result.extend(get_books_data(page_url))
-    return result
-
-
-def main():
+def get_books_data_in_new_window(page_num):
+    print(f"start {page_num}")
+    # options = webdriver.ChromeOptions()
+    # options.add_argument(f"user-agent={UserAgent().chrome}")
+    # options.add_argument("--disable-blink-features=AutomationControlled")
+    # options.add_argument("--headless")
+    curr_proxy = proxy[page_num-1]
+    proxy_options = {
+        "proxy": {
+            "https": f"http://{curr_proxy['login']}:{curr_proxy['password']}@{curr_proxy['server']}"
+        }
+    }
+    driver = webdriver.Chrome(
+        service=service,
+        seleniumwire_options=proxy_options,
+        options=options
+    )
+    drivers.append(driver)
     try:
-        # open_new_tab(url)
-        driver.get(url)
-        login_to_site()
-        get_all_books_data()
+        driver.get(auth_url)
+        login_to_site(driver, url)
+        if page_num != 1:
+            elem = driver.find_element(By.XPATH, f"//a[contains(@class, 'pg')][text()='{page_num}']")
+            driver.get(elem.get_attribute("href"))
+        return get_books_data(driver)
     except Exception as ex:
-        print(ex)
+        print(f"{page_num}:\n{ex}")
     finally:
         driver.close()
         driver.quit()
+        print(f"finish {page_num}")
+    return []
+
+
+def get_all_books_data(driver):
+    result = []
+    # pages = get_search_pages(driver)
+    pages = list(range(1, 3))
+    pool = Pool(processes=len(pages))#len(pages)
+    res = pool.map(get_books_data_in_new_window, pages)
+    result = flatten(res)
+    # for page_url in pages:
+    #     result.extend(get_books_data_in_new_window(page_url))
+    return result
+
+
+def flatten(t):
+    return [item for sublist in t for item in sublist]
+
+
+def main():
+    curr_proxy = proxy[0]
+    proxy_options = {
+        "proxy": {
+            "https": f"http://{curr_proxy['login']}:{curr_proxy['password']}@{curr_proxy['server']}"
+        }
+    }
+    main_driver = webdriver.Chrome(
+        service=service,
+        seleniumwire_options=proxy_options,
+        options=options
+    )
+    drivers.append(main_driver)
+    try:
+        # open_new_tab(url)
+        main_driver.get(url)
+        login_to_site(main_driver, url)
+        get_all_books_data(main_driver)
+    except Exception as ex:
+        print(ex)
+    finally:
+        main_driver.close()
+        main_driver.quit()
 
 
 if __name__ == '__main__':
--- chromeDriver/proxy_auth_data.py
+++ chromeDriver/proxy_auth_data.py
@@ -1,4 +1,12 @@
 # https://proxy6.net/
-server = "91.239.22.122:8000"
-login = "BF0fZ3"
-password = "L0z4Js"
+proxy = [
+    {"server": "91.239.22.122:8000", "login": "BF0fZ3", "password": "L0z4Js"},
+    {"server": "217.29.62.214:38377", "login": "T6wSTM", "password": "BCoD9n"}
+]
+# server = "91.239.22.122:8000"
+# login = "BF0fZ3"
+# password = "L0z4Js"
+
+server = "217.29.62.214:38377"
+login = "T6wSTM"
+password = "BCoD9n"
