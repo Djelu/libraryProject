2d900fa4fd0e487244e9908ca21e9ea8e7738477
izhukov <izhukov@btlab.ru>
2022-01-06 14:17:24 +0300

    .

---
+++ src/main/java/com/fantasy/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
